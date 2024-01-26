cb1b9b12484fdd758dce92a5b1114a361a18b1f8
Chirasi Amaya <87434721+chirasi99@users.noreply.github.com>
2024-01-26 15:35:59 +0530

    update navigation tab

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -1,5 +1,10 @@
+import Image from 'next/image';
+
+
 const TopLeftImg = () => {
-  return <div>Top Left Image</div>;
+  return <div className='absolute top-0 left-0 z-10 mix-blend-color-dodge w-[200px] xl:w-[400px] opacity-50'>
+    <Image src='/top-left-img.png' width={400} height={400} alt='' />
+  </div>;
 };
 
 export default TopLeftImg;
