9e78240446f2996603461f1589683c568150fb12
Chirasi Amaya <87434721+chirasi99@users.noreply.github.com>
2024-01-28 17:45:05 +0530

    edit nav bar

--- components/Nav.js
+++ components/Nav.js
@@ -33,14 +33,32 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 const Nav = () => {
-  return <nav>
+  const router = useRouter();
+  const pathname = router.pathname;
+  return (
+  <nav className='fixed bottom-0 flex flex-col items-center mt-auto xl:justify-center gap-y-4 h-max xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
     {/* inner  */}
-    <div>
+      <div className='flex items-center justify-between w-full px-4 py-8 text-3xl bg-white/10 h-[80px] xl:flex-col xl:justify-center gap-y-10 md:px-40 xl:px-0 xl:h-max backdrop-blur-sm xl:text-xl xl:rounded-full'>
       {navData.map((link, index) =>{
-        return <Link href={link.path}> {link.icon} </Link>;
+        return ( <Link 
+        className={`${link.path===pathname && 'text-accent'} relative flex items-center group hover:text-accent transition-all`}
+        href={link.path} key={index}>
+          {/* tooltip */}
+          <div className='absolute right-0 hidden pr-14 xl:group-hover:flex'>
+            <div className='relative flex items-center bg-white text-primary p-[6px] rounded-[3px]'>
+               <div className='text-[15px] leading-none font-semibold capitalize'>{link.name}</div>
+            </div>
+            {/* triangle */}
+            <div className='border-l-8 border-r-0 border-solid absolute-right-2 border-y-transparent border-l-white'></div>
+          </div>
+          
+          {/* icons */}
+          <div> {link.icon} </div>
+           </Link> );
       })}
     </div>
-  </nav>;
+  </nav>
+  );
 };
 
 export default Nav;
