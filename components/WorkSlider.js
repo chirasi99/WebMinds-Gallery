a2a96cf2366e66e35b305481bfc7313c46ab82e2
Chirasi Amaya <87434721+chirasi99@users.noreply.github.com>
2024-02-23 20:40:43 +0530

    create contact page, work pahe and testimonials

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,5 +1,21 @@
-// data
-const workSlides = {
+// import swiper react components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+//import required modules
+import SwiperCore, { Pagination } from "swiper/core";
+
+import Image from "next/image";
+// import swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+// icons
+import {BsArrowRight} from "react-icons/bs";
+
+SwiperCore.use([Pagination]);
+
+export const workSlider = {
   slides: [
     {
       images: [
@@ -45,7 +61,48 @@ const workSlides = {
 };
 
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+  return (
+    <Swiper
+      spaceBetween={10}
+      pagination={{
+        clickable: true,
+      }}
+      className="h-[280px] sm:h-[480px]">
+      {WorkSlider.slides.map((slide , index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
+              {slide.images.map((image, index) => {
+                  return(<div className="relative flex items-center justify-center overflow-hidden rounded-lg group " key={index}>
+                    <div className="relative flex items-center justify-center overflow-hidden group">
+                      <Image src={image.path} width ={500} height={300} alt={image.title} />
+                      {/* overlay gradient  */}
+                      <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700">
+                        
+                      </div>
+                      {/* title */}
+                        <div className="absolute bottom-0 transition-all duration-300 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20">
+                          <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                          <div className="delay-100">title part 1</div>
+                          <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
+                            PROJECT
+                          </div>
+                          <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
+                            <BsArrowRight />
+                          </div>  
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  );
+              })}
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default WorkSlider;
+
--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,5 +1,60 @@
+// testimonial data
+export const testimonialSlider = [
+  {
+    image: '/t-avt-1.png',
+    name: 'Anne Smith',
+    position: 'Customer',
+    message:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+  },
+  {
+    image: '/t-avt-2.png',
+    name: 'Jane Doe',
+    position: 'Customer',
+    message:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+  },
+  {
+    image: '/t-avt-3.png',
+    name: 'Jhon Doe',
+    position: 'Customer',
+    message:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+  },
+];
+
+// components 
+import TestimonialSlider from "../../components/TestimonialSlider";
+
+// framer motion
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+  <div className="h-full py-32 text-center bg-primary/30">
+    <div className="container flex flex-col justify-center h-full mx-auto">
+      {/* title  */}
+      <motion.h2 
+      variants={fadeIn('up', 0.2)}
+      initial="hidden"
+      animate="show"
+      exit="hidden"
+      className="mb-8 h2 xl:mb-0">
+        What clients <span className="text-accent">say.</span>
+      </motion.h2>
+      {/* slider */}
+      <motion.div
+       variants={fadeIn('up', 0.4)}
+       initial="hidden"
+       animate="show"
+       exit="hidden"
+      >
+        <TestimonialSlider />
+      </motion.div>
+    </div>
+  </div>
+  );
 };
 
 export default Testimonials;
